eda9acf6b2ca1b609aad34e3fcaf5aa55c4ceb30
DevExpressExampleBot <fakeEmail>
2023-10-10 15:17:37 +0400

    Source auto update [skip ci]

--- VB/Dashboard_CreatePies/Form1.Designer.vb
+++ VB/Dashboard_CreatePies/Form1.Designer.vb
@@ -1,61 +1,58 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Namespace Dashboard_CreatePies
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.dashboardViewer1 = New DevExpress.DashboardWin.DashboardViewer(Me.components)
-			CType(Me.dashboardViewer1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' dashboardViewer1
-			' 
-			Me.dashboardViewer1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.dashboardViewer1.Location = New System.Drawing.Point(0, 0)
-			Me.dashboardViewer1.Name = "dashboardViewer1"
-			Me.dashboardViewer1.Size = New System.Drawing.Size(1049, 637)
-			Me.dashboardViewer1.TabIndex = 0
-'			Me.dashboardViewer1.DataLoading += New DevExpress.DataAccess.DataLoadingEventHandler(Me.dashboardViewer1_DataLoading);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(1049, 637)
-			Me.Controls.Add(Me.dashboardViewer1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.dashboardViewer1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.dashboardViewer1 = New DevExpress.DashboardWin.DashboardViewer(Me.components)
+            CType((Me.dashboardViewer1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' dashboardViewer1
+            ' 
+            Me.dashboardViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.dashboardViewer1.Location = New System.Drawing.Point(0, 0)
+            Me.dashboardViewer1.Name = "dashboardViewer1"
+            Me.dashboardViewer1.Size = New System.Drawing.Size(1049, 637)
+            Me.dashboardViewer1.TabIndex = 0
+            AddHandler Me.dashboardViewer1.DataLoading, New DevExpress.DataAccess.DataLoadingEventHandler(AddressOf Me.dashboardViewer1_DataLoading)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(1049, 637)
+            Me.Controls.Add(Me.dashboardViewer1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.dashboardViewer1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents dashboardViewer1 As DevExpress.DashboardWin.DashboardViewer
-	End Class
+'#End Region
+        Private dashboardViewer1 As DevExpress.DashboardWin.DashboardViewer
+    End Class
 End Namespace
-
--- VB/Dashboard_CreatePies/Form1.vb
+++ VB/Dashboard_CreatePies/Form1.vb
@@ -1,53 +1,47 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows.Forms
 Imports DevExpress.DashboardCommon
 Imports DevExpress.DataAccess
 
 Namespace Dashboard_CreatePies
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Function CreatePies(ByVal dataSource As DataSource) As PieDashboardItem
 
-			' Creates a pie dashboard item and specifies its data source.
-			Dim pies As New PieDashboardItem()
-			pies.DataSource = dataSource
+    Public Partial Class Form1
+        Inherits Form
 
-			' Specifies a measure that provides data used to calculate pie values.
-			pies.Values.Add(New Measure("Extended Price"))
-			' Specifies a dimension that provides data for arguments in a pie.
-			pies.Arguments.Add(New Dimension("Country"))
-			' Specifies a dimension that provides data for pie series.
-			pies.SeriesDimensions.Add(New Dimension("OrderDate"))
-
-			Return pies
-		End Function
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-
-			' Creates a dashboard and sets it as the currently opened dashboard in the dashboard viewer.
-			dashboardViewer1.Dashboard = New Dashboard()
-
-			' Creates a data source and adds it to the dashboard data source collection.
-			Dim dataSource As New DataSource("Sales Person")
-			dashboardViewer1.Dashboard.DataSources.Add(dataSource)
-
-			' Creates a pie dashboard item with the specified data source 
-			' and adds it to the Items collection to display within the dashboard.
-			Dim pies As PieDashboardItem = CreatePies(dataSource)
-			dashboardViewer1.Dashboard.Items.Add(pies)
-
-			' Reloads data in the data sources.
-			dashboardViewer1.ReloadData()
-		End Sub
-        Private Sub dashboardViewer1_DataLoading(ByVal sender As Object, _
-                                                 ByVal e As DataLoadingEventArgs) _
-                                             Handles dashboardViewer1.DataLoading
-
-            ' Specifies data for the current data source.
-            e.Data = (New nwindDataSetTableAdapters.SalesPersonTableAdapter()).GetData()
+        Public Sub New()
+            InitializeComponent()
         End Sub
-	End Class
+
+        Private Function CreatePies(ByVal dataSource As DataSource) As PieDashboardItem
+            ' Creates a pie dashboard item and specifies its data source.
+            Dim pies As PieDashboardItem = New PieDashboardItem()
+            pies.DataSource = dataSource
+            ' Specifies a measure that provides data used to calculate pie values.
+            pies.Values.Add(New Measure("Extended Price"))
+            ' Specifies a dimension that provides data for arguments in a pie.
+            pies.Arguments.Add(New Dimension("Country"))
+            ' Specifies a dimension that provides data for pie series.
+            pies.SeriesDimensions.Add(New Dimension("OrderDate"))
+            Return pies
+        End Function
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ' Creates a dashboard and sets it as the currently opened dashboard in the dashboard viewer.
+            dashboardViewer1.Dashboard = New Dashboard()
+            ' Creates a data source and adds it to the dashboard data source collection.
+            Dim dataSource As DataSource = New DataSource("Sales Person")
+            dashboardViewer1.Dashboard.DataSources.Add(dataSource)
+            ' Creates a pie dashboard item with the specified data source 
+            ' and adds it to the Items collection to display within the dashboard.
+            Dim pies As PieDashboardItem = CreatePies(dataSource)
+            dashboardViewer1.Dashboard.Items.Add(pies)
+            ' Reloads data in the data sources.
+            dashboardViewer1.ReloadData()
+        End Sub
+
+        Private Sub dashboardViewer1_DataLoading(ByVal sender As Object, ByVal e As DataLoadingEventArgs)
+            ' Specifies data for the current data source.
+            e.Data =(New nwindDataSetTableAdapters.SalesPersonTableAdapter()).GetData()
+        End Sub
+    End Class
 End Namespace
